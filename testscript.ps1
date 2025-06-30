309d5caaf24f78dcf3ac051dee5898255c2fa8be
Vanessa <112146207+VanessaMancia@users.noreply.github.com>
2025-06-30 13:45:33 -0700

    Update testscript.ps1

--- testscript.ps1
+++ testscript.ps1
@@ -1,11 +1,18 @@
 # testscript.ps1
 
-Write-Output "This is a harmless test to trigger threat protection using EICAR string."
+Write-Output "Starting Software Update..."
 
-# EICAR standard test string
+# Simulate a fake update message
+Add-Type -AssemblyName PresentationFramework
+[System.Windows.MessageBox]::Show("A critical software update is required for your system. Click OK to download and install the update now.","System Update",0)
+
+# Wait a few seconds to look realistic
+Start-Sleep -Seconds 5
+
+Write-Output "Fake software update completed."
+
+# Now place the EICAR test string to simulate malicious behavior
 $eicarString = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
-
-# Write the EICAR string to a file - this will trigger AV
 Set-Content -Path "C:\ProgramData\eicar.com" -Value $eicarString
 
 Write-Output "EICAR test file written to C:\ProgramData\eicar.com"
